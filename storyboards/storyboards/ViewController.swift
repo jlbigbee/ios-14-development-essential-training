54bb0eb7cfb181d696dfba954a5f352706abef7e
Jonathan Bigbee <bigbee.jonathan@gmail.com>
2023-02-28 19:24:21 -0800

    handled a button press

--- storyboards/storyboards/ViewController.swift
+++ storyboards/storyboards/ViewController.swift
@@ -13,9 +13,12 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
-        label.text = "New Text"
     }
 
 
+    @IBAction func buttonPressed(_ sender: Any) {
+        label.text = "New Text"
+        
+    }
 }
 
