eeaca79d6fb00bcfdc4159776f69feca2a06f967
Jonathan Bigbee <bigbee.jonathan@gmail.com>
2023-03-03 12:41:13 -0800

    used a slider in SwiftUI

--- SwiftUIBasics/SwiftUIBasics/ContentView.swift
+++ SwiftUIBasics/SwiftUIBasics/ContentView.swift
@@ -8,13 +8,21 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @State var red = 0.0
+    var green = 0.0
+    var blue = 0.0
+    
+    
     var body: some View {
         VStack {
+            Text("Text 1")
+                .padding()
+                .font(.largeTitle)
             HStack{
-                Image(systemName: "gamecontroller.fill").foregroundColor(.green).font(/*@START_MENU_TOKEN@*/.largeTitle/*@END_MENU_TOKEN@*/)
-                Text("Text 1")
-                    .padding()
-                    .font(.largeTitle)
+                Image(systemName: "gamecontroller.fill").foregroundColor(Color(red: red, green: green, blue: blue, opacity:1)).padding(.leading).font(/*@START_MENU_TOKEN@*/.largeTitle/*@END_MENU_TOKEN@*/)
+                Slider(value: $red, in: 0...1, step: 0.01)
+                    .padding(.trailing)
             }
             HStack {
                 Text("Text 2")
