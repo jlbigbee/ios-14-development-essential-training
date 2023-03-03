6fa48a1175ca01e0987a3d9470a31012c91ce888
Jonathan Bigbee <bigbee.jonathan@gmail.com>
2023-03-03 12:12:48 -0800

    completed SwiftUI challenge

--- SwiftUIBasics/SwiftUIBasics/ContentView.swift
+++ SwiftUIBasics/SwiftUIBasics/ContentView.swift
@@ -10,11 +10,13 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         VStack {
-            Text("Text 1")
-                .padding()
-                .font(.largeTitle)
+            HStack{
+                Image(systemName: "gamecontroller.fill").foregroundColor(.green).font(/*@START_MENU_TOKEN@*/.largeTitle/*@END_MENU_TOKEN@*/)
+                Text("Text 1")
+                    .padding()
+                    .font(.largeTitle)
+            }
             HStack {
-                Image(systemName: "gamecontroller.fill").foregroundColor(.blue)
                 Text("Text 2")
                 Text("Text 3")
             }
