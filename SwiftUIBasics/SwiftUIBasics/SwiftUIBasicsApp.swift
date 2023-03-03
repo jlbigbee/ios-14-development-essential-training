7f911b92525a114efe75a6e2704009cd184758e7
Jonathan Bigbee <bigbee.jonathan@gmail.com>
2023-03-02 22:53:37 -0800

    created iOS app via SwiftUI

---
+++ SwiftUIBasics/SwiftUIBasics/SwiftUIBasicsApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIBasicsApp.swift
+//  SwiftUIBasics
+//
+//  Created by Jonathan Bigbee on 3/2/23.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIBasicsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
