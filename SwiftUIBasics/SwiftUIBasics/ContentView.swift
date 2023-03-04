3131f4b7eaa03bc277960aa1dfbeaf0c9205a877
Jonathan Bigbee <bigbee.jonathan@gmail.com>
2023-03-04 13:04:13 -0800

    built a color picker app

--- SwiftUIBasics/SwiftUIBasics/ContentView.swift
+++ SwiftUIBasics/SwiftUIBasics/ContentView.swift
@@ -10,19 +10,21 @@ import SwiftUI
 struct ContentView: View {
     
     @State var red = 0.0
-    var green = 0.0
-    var blue = 0.0
+    @State var green = 0.0
+    @State var blue = 0.0
     
     
     var body: some View {
         VStack {
-            Text("Text 1")
+            Text("Color Picker")
                 .padding()
                 .font(.largeTitle)
             
                 Image(systemName: "gamecontroller.fill").foregroundColor(Color(red: red, green: green, blue: blue, opacity:1)).padding().font(/*@START_MENU_TOKEN@*/.largeTitle/*@END_MENU_TOKEN@*/)
             
             ColorSliderView(colorValue: $red, color: .red)
+            ColorSliderView(colorValue: $green, color: .green)
+            ColorSliderView(colorValue: $blue, color: .blue)
             
         }
         
