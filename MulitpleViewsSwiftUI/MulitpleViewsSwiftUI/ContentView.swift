2eaf8146394a5df47ca52c611eaefae598409511
Jonathan Bigbee <bigbee.jonathan@gmail.com>
2023-03-04 13:19:17 -0800

    used list views

--- MulitpleViewsSwiftUI/MulitpleViewsSwiftUI/ContentView.swift
+++ MulitpleViewsSwiftUI/MulitpleViewsSwiftUI/ContentView.swift
@@ -9,7 +9,11 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!").padding()
+        NavigationView{
+            List(DataModel.data, id: \.self) { object in
+                Text(object.title)
+            }
+        }
     }
 }
 struct ContentView_Previews: PreviewProvider {
