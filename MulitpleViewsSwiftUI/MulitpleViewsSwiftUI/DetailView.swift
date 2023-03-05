b4938c0329e3f021eeed0ef91c25b9d0e1108a07
Jonathan Bigbee <bigbee.jonathan@gmail.com>
2023-03-05 13:05:12 -0800

    created detail views

---
+++ MulitpleViewsSwiftUI/MulitpleViewsSwiftUI/DetailView.swift
@@ -0,0 +1,20 @@
+//
+//  DetailView.swift
+//  MulitpleViewsSwiftUI
+//
+//  Created by Jonathan Bigbee on 3/5/23.
+//
+
+import SwiftUI
+
+struct DetailView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct DetailView_Previews: PreviewProvider {
+    static var previews: some View {
+        DetailView()
+    }
+}
